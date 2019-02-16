9d63766dde13d78bd24c8782ad2aadb3b21154a8
Giovane Morbi <gemvane@msn.com>
2019-02-16 10:32:07 -0200

    front

---
+++ lista_cidades.js
@@ -0,0 +1,17 @@
+function getcidades(){
+
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://api-ldc-hackathon.herokuapp.com/api/ubs/1', true);
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  }
+
+// Send request
+request.send();
+
+}
