e1d5233c212211c7614adf4123704fd31b4afc8d
fogacalu <rfogaca.lu@gmail.com>
2019-02-16 13:57:45 -0200

    url_atualizada

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -3,6 +3,6 @@ require 'selenium-webdriver'
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
-    config.app_host = 'https://team3/template/pages/tables'
+    config.app_host = 'https://drbrown.local/team3/template/pages/tables'
     config.default_max_wait_time = 5
-end    
+end
